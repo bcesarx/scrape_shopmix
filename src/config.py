070a0fe838d8e8263141d9739f42ff631e06703a
Bruno Cesar Silva de Menezes <bcesar@gmail.com>
2023-07-13 19:29:21 -0300

    Update config.py

---
+++ src/config.py
@@ -0,0 +1,16 @@
+### instanciar a url contendo todos os links do site
+URL_INDEX = "https://www.gruposhopmix.com/sitemap.xml"
+###### acesso ao banco de dados
+
+
+p_host = 'localhost'
+p_port = 5432
+db = 'postgres'
+ssh = True
+ssh_user = 'ubuntu'
+ssh_host = '144.22.150.9'
+psql_user = 'postgres'
+psql_pass = 'alice11'
+ssh_pkey = r"C:\Users\bcesa\OneDrive\Documentos\Infra na Núvem\mydata\ssh-key-2022-10-28.key"
+
+#############################################
