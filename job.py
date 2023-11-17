6bee011eaf973a65f31858229a5892a3b1d4a29c
Bruno Cesar Silva de Menezes <58083694+bcesarx@users.noreply.github.com>
2023-11-17 15:19:08 -0300

    Update job.py

--- job.py
+++ job.py
@@ -1,7 +1,4 @@
 # %%
-
-
-# %%
 # Importar bibiotecas
 import pandas as pd
 import requests
@@ -12,7 +9,12 @@ import time
 from datetime import datetime
 from tqdm import tqdm
 from src import dw_utils as dw
+
+# %%
 import platform
+
+# %%
+platform.system()
 
 # %%
 ### atualzar lista de links no sitemap
@@ -28,7 +30,7 @@ all_locs = soup.find_all('loc')
 urls = pd.DataFrame(columns = ["links_catalogo"], data=all_locs)
 
 ### remove keywords from urls
-keywords = ['pagina','sao-paulo', 'todos-os-produtos', 'os-mais-vendidos', 'novidades']
+keywords = ['pagina','sao-paulo', 'todos-os-produtos', 'os-mais-vendidos', 'novidades','black-dos-campeoes']
 urls = urls[~urls["links_catalogo"].str.contains('|'.join(keywords))]
 ## drop lines that is equal to the base url
 urls = urls.loc[urls["links_catalogo"] != BASE_URL]
@@ -36,13 +38,15 @@ urls = urls.loc[urls["links_catalogo"] != BASE_URL]
 urls.reset_index(inplace=True, drop=True)
 
 # %%
+urls
+
+# %%
 
 def extract_data_from_html(soup):
     
-    print('Extracting data from html', link)
+    
     nome = soup.find('meta', property='og:title')['content']
-    try:
-        codigo = soup.find('meta', attrs={'name': 'twitter:data1'})['content']
+    codigo = soup.find('meta', attrs={'name': 'twitter:data1'})['content']
     try:
         preco = soup.find('strong', attrs = 'preco-promocional cor-principal titulo')['data-sell-price']
     except TypeError:
@@ -58,7 +62,8 @@ def extract_data_from_html(soup):
     except AttributeError:
         estoque = 'Não disponível'
     imagens = [img['src'] for img in soup.find_all('img') if img.get('src') != None]
-    imagens = [imagem for imagem in imagens if 'produto' in imagem]    
+    imagens = [imagem for imagem in imagens if 'produto' in imagem]
+    
     return {
         'nome': nome,
         'código': codigo,
@@ -80,6 +85,7 @@ for link in tqdm(urls["links_catalogo"], desc='Obtendo dados dos produtos', unit
 
     ## baixar pagina do produto
     prod_content= requests.get(link).text
+    
     ### converter página em soup
     prod_content_soup = bs(prod_content, 'html.parser')
     ### extrair dicionário com os dados chave do produto ## converter dicionário em dataframe
@@ -91,6 +97,7 @@ for link in tqdm(urls["links_catalogo"], desc='Obtendo dados dos produtos', unit
     df_produto = pd.DataFrame(dict_produto, index=[0])
     ###inserir o link do produto
     df_produto["link"] = link
+    
     ### inseirr a data de extração
     df_produto["data_extracao"] = datetime.now()
     ## adicionar ao dataframe consolidado
@@ -101,7 +108,7 @@ for link in tqdm(urls["links_catalogo"], desc='Obtendo dados dos produtos', unit
     if counter  == 201:
         time.sleep(57)
         counter = 0
-    break
+    
 
 # salvar dataframe consolidado em csv
 
@@ -112,6 +119,8 @@ for link in tqdm(urls["links_catalogo"], desc='Obtendo dados dos produtos', unit
 
 
 # %%
+### carregar no banco
+
 
 if platform.system() == 'Windows':
     p_host = 'localhost'
@@ -148,18 +157,14 @@ elif platform.system() == 'Linux':
     ssh_pkey = r"/home/ubuntu/ssh-key-2022-10-28.key"
     
     pgres = dw.Postgresql_connect(pgres_host=p_host, pgres_port=p_port, db=db, ssh=ssh, ssh_user=ssh_user, ssh_host=ssh_host, ssh_pkey=ssh_pkey, psql_user=psql_user
-                                , psql_pass=psql_pass)#initiates a connection to the PostgreSQL database. In this instance we use ssh and must specify our ssh credentials.
-
-#You'll need to define psql_user and psql_pass using input() and getpass() to temporarily store your credentials.
-#Alternatively, best practice you may be to store your credentials as environment variables.
-# psql_user = input("Please enter your database username:")
-
+                                , psql_pass=psql_pass)
 
 # %%
 #df_produtos_consolidado = pd.read_csv('data/produtos_consolidado.csv', sep='|')
 
 # %%
 ### criar conexão com o banco de dados
+## atualizar tabela
 
 pgres.replace_table(df_produtos_consolidado, 'catalogo_shopmix')
 
@@ -169,15 +174,15 @@ pgres.replace_table(df_produtos_consolidado, 'catalogo_shopmix')
 # %%
 
 
-sql_statement = """
-SELECT *
-FROM catalogo_shopmix
-;
-"""
-query_df = pgres.query( query=sql_statement)
-query_df
-#returns the results of an sql statement as a pandas dataframe. 
-#This example returns the column names and data types of table 'ey_test_table'.
+# sql_statement = """
+# SELECT *
+# FROM catalogo_shopmix
+# ;
+# """
+# query_df = pgres.query( query=sql_statement)
+# query_df
+# #returns the results of an sql statement as a pandas dataframe. 
+# #This example returns the column names and data types of table 'ey_test_table'.
 
 # %%
 
